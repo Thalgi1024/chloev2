60d40d4613d3758bcebdea75ac4e73b54273c10e
justink6 <justink6@illinois.edu>
2023-03-13 13:33:33 -0500

    readimage

--- cogs/db/readimage.py
+++ cogs/db/readimage.py
@@ -4,20 +4,7 @@ import requests
 import pytesseract
 
 def readDataFromImage(url):
-  # Process image
-  im = Image.open(requests.get(url, stream=True).raw)
-  width,height = im.size
-
-  left = 0
-  top = height / 10
-  right = width / 4
-  bottom = height
-
-  im1 = im.crop((left, top, right, bottom))
-
-  thresh = 65
-  fn = lambda x : 255 if x > thresh else 0
-  im1 = im1.convert('L').point(fn, mode='1')
+  im1 = processImage(url)
 
   # Process text from image
   text = pytesseract.image_to_string(im1)
@@ -40,27 +27,27 @@ def readDataFromImage(url):
     tokens = line.split(' ')
     
     # Read in data
-    if token[0] == "Attack":
+    if tokens[0] == "Attack":
       try: 
-        Attack = int(token[1])
+        Attack = int(tokens[1])
       except:
         print("Could not read attack")
         Attack = -1
-    if token[0] == "Defense":
+    if tokens[0] == "Defense":
       try: 
-        Defense = int(token[1])
+        Defense = int(tokens[1])
       except:
         print("Could not read defense")
         Defense = -1
-    if token[0] == "Health":
+    if tokens[0] == "Health":
       try: 
-        Health = int(token[1])
+        Health = int(tokens[1])
       except:
         print("Could not read health")
-        Helath = -1
-    if token[0] == "Speed":
+        Health = -1
+    if tokens[0] == "Speed":
       try: 
-        Speed = int(token[1])
+        Speed = int(tokens[1])
       except:
         print("Could not read speed")
         Speed = -1
@@ -72,3 +59,29 @@ def readDataFromImage(url):
   #print(Name, Attack, Defense, Health, Speed, CChance, CDamage, Effectiveness, EffectResist)
 
   return Attack, Defense, Health, Speed, CChance, CDamage, Effectiveness, EffectResist
+
+def processImage(url):
+  # Process image
+  im = Image.open(requests.get(url, stream=True).raw)
+  width,height = im.size
+
+  left = 0
+  top = height / 10
+  right = width / 4
+  bottom = height
+
+  im1 = im.crop((left, top, right, bottom))
+
+  thresh = 65
+  fn = lambda x : 255 if x > thresh else 0
+  im1 = im1.convert('L').point(fn, mode='1')
+
+  return im1
+
+def cleanText(line):
+  cleanTokens = []
+
+  keywords = ['Attack', 'Defense', 'Health', 'Critical', 'Hit', 'Chance', 'Damage', 'Effectiveness', 'Effect', 'Resistance']
+
+  for token in line:
+    
--- script.py
+++ script.py
@@ -11,4 +11,4 @@ import cogs.db.readimage as readimage
 #  query = "INSERT INTO Names VALUES (?, ?)"
  # db.execute(query, nick, name)
 
-readimage.readDataFromImage("https://cdn.discordapp.com/attachments/1072673382694400051/1073340205232300152/unknown.png")
+readimage.readDataFromImage("https://cdn.discordapp.com/attachments/1072673382694400051/1073342448153141318/unknown.png")
